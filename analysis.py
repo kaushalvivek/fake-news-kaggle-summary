636b0ae37c7d17998e5e361e1bfd3acf1a162e2d
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2020-05-06 15:11:17 +0530

    old scirpt file removed

--- analysis.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-path_to_data_files = '../files/fake-news-kaggle-corpus/'
-submit = pd.read_csv(path_to_data_files+'submit.csv')
-test = pd.read_csv(path_to_data_files+'test.csv')
-train = pd.read_csv(path_to_data_files+'train.csv')
-
-print(train)
